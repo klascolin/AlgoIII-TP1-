5d46b864f863ac6d947f4720fed92c3b0cb06359
nicolas <klascolin@gmail.com>
2015-08-25 22:42:31 -0300

    Agrego la ultima modificacion de separar

--- Ej 3/Separar.java
+++ Ej 3/Separar.java
@@ -1,25 +1,34 @@
-package uba.algo3.ejemplotp;
-
+package ej3;
+import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class Separar {
 	public Character[] exploradora;
-	public String[] amigas;
+	public HashSet<Amistad> amigas;
 	public int cantidad;
 	
 	public Separar(String s){
 		StringTokenizer st=new StringTokenizer(s,";");
-		exploradora=new Character[st.countTokens()];
-		amigas=new String[st.countTokens()];
+		amigas=new HashSet<Amistad>();
 		cantidad=st.countTokens();
-		int i=0;
 		StringTokenizer nt;
+		HashSet<Character> e=new HashSet<Character>();
 		while(st.hasMoreTokens()){
 			nt=new StringTokenizer(st.nextToken());
-			exploradora[i]=nt.nextToken().charAt(0);
-			amigas[i]=nt.nextToken();
-			i++;
+			String g= nt.nextToken();
+			String m=nt.nextToken();
+			int n=0;
+			while(n<m.length()){
+				e.add(m.charAt(n));
+				Amistad r = new Amistad(g.charAt(0),m.charAt(n));
+				boolean b=amigas.add(r);
+				if(b){
+					System.out.println(g.charAt(0)+" "+m.charAt(n));
+				}
+				n++;
+			}
 		}
-		
+		exploradora=(Character[]) e.toArray(new Character[0]);
 	}
+
 }
